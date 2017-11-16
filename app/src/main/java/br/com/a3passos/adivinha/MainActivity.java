99cb04f2ad3b21d934f364939ab96da36fec8345
Eduardo <dudarenz@gmail.com>
2017-11-16 00:20:03 -0200

    primeiro no git

---
+++ app/src/main/java/br/com/a3passos/adivinha/MainActivity.java
@@ -0,0 +1,39 @@
+package br.com.a3passos.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button)findViewById(R.id.btJogar);
+        textoResultado = (TextView) findViewById(R.id.lblNumero);
+
+
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomico = new Random();
+                int numeroSorteado = randomico.nextInt(11);
+                textoResultado.setText("Texto Alterado: " + numeroSorteado);
+            }
+        });
+
+    }
+
+
+}
